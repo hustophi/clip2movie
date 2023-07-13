b185a52449732bfbe54eb57206854f000d27a719
hustophi <zhangheng201747@outlook.com>
2023-07-13 17:43:20 +0800

    add utils

--- utils/util.py
+++ utils/util.py
@@ -1,11 +1,11 @@
 import json
+import math
 import torch
 import pandas as pd
 from pathlib import Path
 from itertools import repeat
 from collections import OrderedDict
-
-
+import matplotlib.pyplot as plt
 def ensure_dir(dirname):
     dirname = Path(dirname)
     if not dirname.is_dir():
@@ -31,6 +31,7 @@ def prepare_device(n_gpu_use):
     setup GPU device if available. get gpu device indices which are used for DataParallel
     """
     n_gpu = torch.cuda.device_count()
+    print("############", n_gpu)
     if n_gpu_use > 0 and n_gpu == 0:
         print("Warning: There\'s no GPU available on this machine,"
               "training will be performed on CPU.")
@@ -42,6 +43,24 @@ def prepare_device(n_gpu_use):
     device = torch.device('cuda:0' if n_gpu_use > 0 else 'cpu')
     list_ids = list(range(n_gpu_use))
     return device, list_ids
+
+def show_heatmaps(matrices, xlabel, ylabel, titles=None, figsize=(5, 5),
+                  cmap='Reds'):
+    """显示矩阵热图"""
+    num_rows, num_cols = matrices.shape[0], matrices.shape[1]
+    fig, axes = plt.subplots(num_rows, num_cols, figsize=figsize,
+                                 sharex=True, sharey=True, squeeze=False)
+
+    for i, (row_axes, row_matrices) in enumerate(zip(axes, matrices)):
+        for j, (ax, matrix) in enumerate(zip(row_axes, row_matrices)):
+            pcm = ax.imshow(matrix.detach().numpy(), cmap=cmap)
+            if i == num_rows - 1:
+                ax.set_xlabel(xlabel)
+            if j == 0:
+                ax.set_ylabel(ylabel)
+            if titles:
+                ax.set_title(titles[j])
+    fig.colorbar(pcm, ax=axes, shrink=0.6)
 
 class MetricTracker:
     def __init__(self, *keys, writer=None):
